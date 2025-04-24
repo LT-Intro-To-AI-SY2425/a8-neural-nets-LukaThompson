883cfe0ee297217b901e54db1a8346b867bc1cf5
LukaThompson <lbthompson@cps.edu>
2025-04-24 10:33:11 -0500

    finished

--- a8.py
+++ a8.py
@@ -1,7 +1,32 @@
 from neural import *
 
-print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
+# print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
+
+# print("\n\nTraining XOR\n\n")
+# xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
+
+# xorn = NeuralNet(2, 8, 1)
+# xorn.train(xor_training_data, iters=10000, print_interval=100)
+# print(xorn.test_with_expected(xor_training_data))
+
+print("<<<<<<<<<<<<<<Political Data>>>>>>>>>>>>>>>>>")
+
+print("\n\nTraning Political\n")
+political_training_data = [
+    ([0.9, 0.6, 0.8, 0.3, 0.1], [1]),
+    ([0.8, 0.8, 0.4, 0.6, 0.4], [1]),
+    ([0.7, 0.2, 0.4, 0.6, 0.3], [1]),
+    ([0.5, 0.5, 0.8, 0.4, 0.8], [0]),
+    ([0.3, 0.1, 0.6, 0.8, 0.8], [0]),
+    ([0.6, 0.3, 0.4, 0.3, 0.6], [0])
+]
 
 
+political_net = NeuralNet(5, 5, 1)
+political_net.train(political_training_data, iters=10000, print_interval=100)
+
+new_input = [0.9, 0.8, 0.8, 0.3, 0.6]
+prediction = political_net.evaluate(new_input)
 
 
+print(f"Prediction for input {new_input}: {prediction}")
